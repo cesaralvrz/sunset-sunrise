353940f2b5fc66511fb3099c3f78984c9b8d8f81
César Álvarez Llaneza <cesaralv97@gmail.com>
2023-06-12 09:35:33 +0200

    Remove dark classes on TheFooter component

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
-	<footer class="px-4 dark:bg-gray-800 dark:text-gray-100">
-		<p class="py-6 text-sm text-center dark:text-gray-400">Copyright © {{ date }} - César Álvarez Llaneza</p>
+	<footer class="px-4 bg-gray-800 text-gray-100">
+		<p class="py-6 text-sm text-center text-gray-400">Copyright © {{ date }} - César Álvarez Llaneza</p>
 	</footer>
 </template>
 
